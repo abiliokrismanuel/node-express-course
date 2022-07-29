b750d1d80922dd0b1655297711fdb61e03b8e596
Abilio Krismanuel <78332696+abiliokrismanuel@users.noreply.github.com>
2022-07-29 22:46:09 +0700

    add get first server route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app =express();
 
+const mockUserData=[
+    {name:'abilio'},
+    {name:'krismanuel'}
+]
+
+app.get('/user',function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users. NICE!',
+        users: mockUserData
+    })
+})
+
 app.listen(8000,function () {
     console.log("server is running")
     
