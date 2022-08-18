bb572722f2db599d42ab2882d92ef0004e2f0f63
Abilio Krismanuel <78332696+abiliokrismanuel@users.noreply.github.com>
2022-08-19 00:39:20 +0700

    add login post route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app =express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 const mockUserData=[
     {name:'abilio'},
@@ -21,6 +24,29 @@ app.get('/users/:id',function(req,res){
         message: 'got one user',
         user:req.params.id
     })
+})
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    //this should come from database
+    const mockUsername="abiliow";
+    const mockPassword="krismanuel";
+    
+    if(username===mockUsername && password===mockPassword){
+        //use json web token
+        res.json({
+            success:true,
+            message:'password and username match',
+            token:'encrypted token goes here'
+        })
+    }else{
+        res.json({
+            success:false,
+            message:'password does not match'
+        })
+    }
 })
 
 app.listen(8000,function () {
